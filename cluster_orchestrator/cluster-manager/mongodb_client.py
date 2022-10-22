d599c8f102a9e4d95d35793cb64cf1e9dd28afdf
Patrick Sabanic <patrick.sabanic@tum.de>
2022-10-22 23:32:46 +0200

    Enable scheduling for different architectures

--- cluster_orchestrator/cluster-manager/mongodb_client.py
+++ cluster_orchestrator/cluster-manager/mongodb_client.py
@@ -110,6 +110,7 @@ def mongo_aggregate_node_information(TIME_INTERVAL):
     cumulative_memory_in_mb = 0
     number_of_active_nodes = 0
     technology = []
+    architecture = []
 
     nodes = find_all_nodes()
     for n in nodes:
@@ -130,7 +131,10 @@ def mongo_aggregate_node_information(TIME_INTERVAL):
                 number_of_active_nodes += 1
                 for t in n.get('node_info').get('technology'):
                     technology.append(t) if t not in technology else technology
-
+                
+                arch = n.get('node_info').get('architecture')
+                if arch is not None:
+                    architecture.append(arch) if arch not in architecture else architecture
             else:
                 print('Node {0} is inactive.'.format(n.get('_id')))
         except Exception as e:
@@ -142,7 +146,7 @@ def mongo_aggregate_node_information(TIME_INTERVAL):
     return {'cpu_percent': cumulative_cpu, 'memory_percent': cumulative_memory,
             'cpu_cores': cumulative_cpu_cores, 'cumulative_memory_in_mb': cumulative_memory_in_mb,
             'gpu_cores': gpu_cores, 'gpu_percent': gpu_percent,
-            'number_of_nodes': number_of_active_nodes, 'jobs': jobs, 'virtualization': technology, 'more': 0}
+            'number_of_nodes': number_of_active_nodes, 'jobs': jobs, 'virtualization': technology, 'arch' : architecture, 'more': 0}
 
 
 # ................. Job Operations .......................#
--- root_orchestrator/system-manager-python/ext_requests/cluster_db.py
+++ root_orchestrator/system-manager-python/ext_requests/cluster_db.py
@@ -78,6 +78,7 @@ def mongo_update_cluster_information(cluster_id, data):
     gpu_percent = data.get('gpu_percent')
     # technology = data.get('technology')
     virtualization = data.get('virtualization')
+    architecture = data.get('arch')
     more = data.get('more')
     worker_groups = data.get('worker_groups')
 
@@ -89,7 +90,8 @@ def mongo_update_cluster_information(cluster_id, data):
         {'$set': {'aggregated_cpu_percent': cpu_percent, 'total_cpu_cores': cpu_cores,
                   'total_gpu_cores': gpu_cores, 'total_gpu_percent': gpu_percent,
                   'aggregated_memory_percent': memory_percent, 'memory_in_mb': memory_in_mb,
-                  'active_nodes': nodes, 'virtualization': virtualization, 'more': more,
+                  'active_nodes': nodes, 'virtualization': virtualization, 'arch': architecture,
+                  'more': more,
                   'last_modified': datetime_now, 'last_modified_timestamp': datetime_now_timestamp,
                   'worker_groups': worker_groups}},
         upsert=True)
